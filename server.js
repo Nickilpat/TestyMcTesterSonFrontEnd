fac48b0017b48fbbba41a797fbc83ecfba9213b5
Nick <nicholas.kilpatrick@gmail.com>
2020-02-22 12:50:02 -0500

    undid vsCode autofix

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //Install express server
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
+// import express, { static } from 'express';
+// import { join } from 'path';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(static(__dirname + '/dist/testyMcTesterSonForntEnd'));
+app.use(express.static(__dirname + '/dist/testyMcTesterSonForntEnd'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(join(__dirname+'/dist/testyMcTesterSonForntEnd/index.html'));
+res.sendFile(path.join(__dirname+'/dist/testyMcTesterSonForntEnd/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
